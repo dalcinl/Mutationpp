9d7a3ce9369d417147e791151a106706e289da72
Lisandro Dalcin <dalcinl@gmail.com>
2025-11-27 09:57:02 +0300

    python: Install data files as package data

--- interface/python/mutationpp/__init__.py
+++ interface/python/mutationpp/__init__.py
@@ -1,1 +1,11 @@
-from ._mutationpp import *
+from ._mutationpp import *  # noqa: F403 # ty: ignore[unresolved-import]
+
+import os
+
+if "MPP_DATA_DIRECTORY" not in os.environ:
+    mpp_directory = os.path.dirname(__file__)
+    mpp_data_directory = os.path.join(mpp_directory, "data")
+    os.environ["MPP_DATA_DIRECTORY"] = mpp_data_directory
+    del mpp_directory, mpp_data_directory
+
+del os
